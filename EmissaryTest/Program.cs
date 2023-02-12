2f42f2c6ec28da605a20f840a5975b2717987cfd
thorhalbert <thorhalbert@gmail.com>
2023-02-12 12:07:30 -0800

    Start figuring out wasmtime.    Looks like we can implement all of the WASI bits ourself.

--- EmissaryTest/Program.cs
+++ EmissaryTest/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace EmissaryTest
+﻿using EmissaryExecutionEnvironment;
+
+namespace EmissaryTest
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var engine = new TestWasm();
+            engine.Launch(@"C:\Projects\HelloWorldEmissary\BouncySphere\bin\Debug\net7.0\BouncySphere.wasm");
+
         }
     }
 }
