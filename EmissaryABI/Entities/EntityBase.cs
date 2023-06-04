296cc240eae2ed8d33789368721e51baa6a7aa56
Thor Halbert <thor@coolearth.com>
2023-06-03 17:18:55 -0700

    Call/Callback interop calls mostly built out.

--- EmissaryABI/Entities/EntityBase.cs
+++ EmissaryABI/Entities/EntityBase.cs
@@ -14,12 +14,14 @@ using EmissaryABIProtos.Callbacks.EntityInstanceVersionUpdate;
 using EmissaryABIProtos.Callbacks.InstantiateEntityInstance;
 using EmissaryABIProtos.Callbacks.ReceiveInstanceStateChange;
 using EmissaryABIProtos.Callbacks.ReceiveMessageFromChannel;
+using EmissaryABIProtos.Calls;
 using EmissaryABIProtos.Calls.AlterEntityInstance;
 using EmissaryABIProtos.Calls.ApplyPhysicsToEntityInstance;
 using EmissaryABIProtos.Calls.GenerateStateChange;
 using EmissaryABIProtos.Calls.RegisterEntityHandlers;
 using EmissaryABIProtos.Calls.RequestDestroyEntityInstance;
 using EmissaryABIProtos.Calls.SpawnNewEntityInstance;
+using EmissaryABIProtos.Messages.EntityIdentity;
 using EmissaryABIProtos.Messages.SubscribeToChannel;
 
 namespace EntityABI.Entities
@@ -40,42 +42,121 @@ namespace EntityABI.Entities
 
         protected int DispatchOffset { get; private set; }      // We assign this, not the derrived class
 
+        protected EntityIdentity Identity { get; private set; }
+
+        protected EntityBase(EntityIdentity entityIdentity)
+        {
+            Identity = entityIdentity;
+
+            // Register this
+
+            foreach (var r in EntityRegistry)
+                if (r.Identity == Identity) throw new ArgumentException("Entity Already Exists");
+
+            DispatchOffset = EntityRegistry.Length;  // This is where new will appear on the array
+
+            // Append to the array
+            var registry = EntityRegistry.ToList();
+            registry.Add(this);
+            EntityRegistry = registry.ToArray();   // Array for speed
+
+            // call interop to register the entity handler
+            var ret = EntityABI_RegisterEntityHandlers(new RegisterEntityHandlersCall
+            {
+                NewDispatchOffset = DispatchOffset
+            });
+
+            // Register will eventually return other stuff for us which we can save for the entity implementation
+        }
+
         #region ABI Calls
 
         // Call via interop
 
-        protected  AlterEntityInstanceReturn AlterEntityInstance(AlterEntityInstanceCall call)
+        private ABI_Call_Returns PerformABICall(ABI_Call_Calls abiCall)
         {
-            return new AlterEntityInstanceReturn();
+            abiCall.DispatchOffset = DispatchOffset;
+
+            // Call happens here - dummy until we get this 
+
+            return new ABI_Call_Returns();
         }
 
-        protected  ApplyPhysicsToEntityInstanceReturn ApplyPhysicsToEntityInstance(ApplyPhysicsToEntityInstanceCall call)
+        protected AlterEntityInstanceReturn EntityABI_AlterEntityInstance(AlterEntityInstanceCall call)
         {
-            return new ApplyPhysicsToEntityInstanceReturn();
+            var abiCall = new ABI_Call_Calls
+            {
+                AlterEntityInstanceCall = call
+            };
+
+            var ret = PerformABICall(abiCall);
+            return ret.AlterEntityInstanceReturn; 
         }
 
-        protected RequestDestroyEntityInstanceReturn RequestDestroyEntityInstance(RequestDestroyEntityInstanceCall call)
+        protected ApplyPhysicsToEntityInstanceReturn EntityABI_ApplyPhysicsToEntityInstance(ApplyPhysicsToEntityInstanceCall call)
         {
-            return new RequestDestroyEntityInstanceReturn();
+            var abiCall = new ABI_Call_Calls
+            {
+                ApplyPhysicsToEntityInstanceCall = call
+            };
+
+            var ret = PerformABICall(abiCall);
+            return ret.ApplyPhysicsToEntityInstanceReturn;
         }
 
+        protected RequestDestroyEntityInstanceReturn EntityABI_RequestDestroyEntityInstance(RequestDestroyEntityInstanceCall call)
+        {
+            var abiCall = new ABI_Call_Calls
+            {
+                RequestDestroyEntityInstanceCall = call
+            };
 
-        protected GenerateStateChangeReturn GenerateStateChange(GenerateStateChangeCall call)
-        {
-            return new GenerateStateChangeReturn();
+            var ret = PerformABICall(abiCall);
+            return ret.RequestDestroyEntityInstanceReturn;
         }
 
-        protected  RegisterEntityHandlersReturn RegisterEntityHandlers(RegisterEntityHandlersCall call)
+        protected GenerateStateChangeReturn EntityABI_GenerateStateChange(GenerateStateChangeCall call)
         {
-            return new RegisterEntityHandlersReturn();
+            var abiCall = new ABI_Call_Calls
+            {
+                GenerateStateChangeCall = call
+            };
+
+            var ret = PerformABICall(abiCall);
+            return ret.GenerateStateChangeReturn;
         }
-        protected  SpawnNewEntityInstanceReturn SpawnNewEntityInstance(SpawnNewEntityInstanceCall call)
+
+        protected RegisterEntityHandlersReturn EntityABI_RegisterEntityHandlers(RegisterEntityHandlersCall call)
         {
-            return new SpawnNewEntityInstanceReturn();
+            var abiCall = new ABI_Call_Calls
+            {
+                RegisterEntityHandlersCall = call
+            };
+
+            var ret = PerformABICall(abiCall);
+            return ret.RegisterEntityHandlersReturn;
         }
-        protected  SubscribeToChannelReturn SubscribeToChannel(SubscribeToChannelCall call)
+
+        protected SpawnNewEntityInstanceReturn EntityABI_SpawnNewEntityInstance(SpawnNewEntityInstanceCall call)
         {
-            return new SubscribeToChannelReturn();
+            var abiCall = new ABI_Call_Calls
+            {
+                SpawnNewEntityInstanceCall = call
+            };
+
+            var ret = PerformABICall(abiCall);
+            return ret.SpawnNewEntityInstanceReturn;
+        }
+
+        protected SubscribeToChannelReturn EntityABI_SubscribeToChannel(SubscribeToChannelCall call)
+        {
+            var abiCall = new ABI_Call_Calls
+            {
+                SubscribeToChannelCall = call
+            };
+
+            var ret = PerformABICall(abiCall);
+            return ret.SubscribeToChannelReturn;
         }
 
 
@@ -85,14 +166,45 @@ namespace EntityABI.Entities
 
         // We will be called via interop and will dispatch via DispatchOffset (virtual functions)
 
-        public virtual DestroyEmissaryReturn DestroyEmissary(DestroyEmissaryCall call) { return new(); }
-        public virtual DestroyEntityHandlerReturn DestroyEntityHandler(DestroyEntityHandlerCall call) { return new(); }
-        public virtual DestroyEntityInstanceReturn DestroyEntityInstance(DestroyEntityInstanceCall call) { return new(); }
-        public virtual EntityInstanceUpdateReturn EntityInstanceUpdate(EntityInstanceUpdateCall call) { return new(); }
-        public virtual EntityInstanceVersionUpdateReturn EntityInstanceVersionUpdate(EntityInstanceVersionUpdateCall call) { return new(); }
-        public virtual InstantiateEntityInstanceReturn InstantiateEntityInstance(InstantiateEntityInstanceCall call) { return new(); }
-        public virtual ReceiveInstanceStateChangeReturn ReceiveInstanceStateChange(ReceiveInstanceStateChangeCall call) { return new(); }
-        public virtual ReceiveMessageFromChannelReturn ReceiveMessageFromChannel(ReceiveMessageFromChannelCall call) { return new(); }
+        public virtual DestroyEmissaryReturn ABICallback_DestroyEmissary(DestroyEmissaryCall call)
+        {
+            return new();
+        }
+
+        public virtual DestroyEntityHandlerReturn ABICallback_DestroyEntityHandler(DestroyEntityHandlerCall call)
+        {
+            return new();
+        }
+
+        public virtual DestroyEntityInstanceReturn ABICallback_DestroyEntityInstance(DestroyEntityInstanceCall call)
+        {
+            return new();
+        }
+
+        public virtual EntityInstanceUpdateReturn ABICallback_EntityInstanceUpdate(EntityInstanceUpdateCall call)
+        {
+            return new();
+        }
+
+        public virtual EntityInstanceVersionUpdateReturn ABICallback_EntityInstanceVersionUpdate(EntityInstanceVersionUpdateCall call)
+        {
+            return new();
+        }
+
+        public virtual InstantiateEntityInstanceReturn ABICallback_InstantiateEntityInstance(InstantiateEntityInstanceCall call)
+        {
+            return new();
+        }
+
+        public virtual ReceiveInstanceStateChangeReturn ABICallback_ReceiveInstanceStateChange(ReceiveInstanceStateChangeCall call)
+        {
+            return new();
+        }
+
+        public virtual ReceiveMessageFromChannelReturn ABICallback_ReceiveMessageFromChannel(ReceiveMessageFromChannelCall call)
+        {
+            return new();
+        }
 
         /// <summary>
         /// Unwrap the callback and call the callback on the proper enty and return the Value
@@ -111,10 +223,28 @@ namespace EntityABI.Entities
             switch (call.CallsCase)
             {
                 case ABI_Callbacks_Calls.CallsOneofCase.DestroyEmissaryCall:
-                    ret.DestroyEmissaryReturn = entity.DestroyEmissary(call.DestroyEmissaryCall);
+                    ret.DestroyEmissaryReturn = entity.ABICallback_DestroyEmissary(call.DestroyEmissaryCall);
                     return ret;
                 case ABI_Callbacks_Calls.CallsOneofCase.DestroyEntityHandlerCall:
-                    ret.DestroyEntityHandlerReturn = entity.DestroyEntityHandler(call.DestroyEntityHandlerCall);
+                    ret.DestroyEntityHandlerReturn = entity.ABICallback_DestroyEntityHandler(call.DestroyEntityHandlerCall);
+                    return ret;
+                case ABI_Callbacks_Calls.CallsOneofCase.DestroyEntityInstanceCall:
+                    ret.DestroyEntityInstanceReturn = entity.ABICallback_DestroyEntityInstance(call.DestroyEntityInstanceCall);
+                    return ret;
+                case ABI_Callbacks_Calls.CallsOneofCase.EntityInstanceUpdateCall:
+                    ret.EntityInstanceUpdateReturn = entity.ABICallback_EntityInstanceUpdate(call.EntityInstanceUpdateCall);
+                    return ret;
+                case ABI_Callbacks_Calls.CallsOneofCase.EntityInstanceVersionUpdateCall:
+                    ret.EntityInstanceVersionUpdateReturn = entity.ABICallback_EntityInstanceVersionUpdate(call.EntityInstanceVersionUpdateCall);
+                    return ret;
+                case ABI_Callbacks_Calls.CallsOneofCase.InstantiateEntityInstanceCall:
+                    ret.InstantiateEntityInstanceReturn = entity.ABICallback_InstantiateEntityInstance(call.InstantiateEntityInstanceCall);
+                    return ret;
+                case ABI_Callbacks_Calls.CallsOneofCase.ReceiveInstanceStateChangeCall:
+                    ret.ReceiveInstanceStateChangeReturn = entity.ABICallback_ReceiveInstanceStateChange(call.ReceiveInstanceStateChangeCall);
+                    return ret;
+                case ABI_Callbacks_Calls.CallsOneofCase.ReceiveMessageFromChannelCall:
+                    ret.ReceiveMessageFromChannelReturn = entity.ABICallback_ReceiveMessageFromChannel(call.ReceiveMessageFromChannelCall);
                     return ret;
             }
 
